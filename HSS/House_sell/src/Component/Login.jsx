2f518797414588a42fb4eefca6140f40a7e8bcea
Inatha1970 <inathaomar@gmail.com>
2025-08-24 00:42:29 -0700

    second

--- HSS/House_sell/src/Component/Login.jsx
+++ HSS/House_sell/src/Component/Login.jsx
@@ -1,8 +1,22 @@
-function Login(){
-    return(
+import { Link } from "react-router-dom";
+
+function Login() {
+    return (
         <> 
-       
+            <div className="login">
+                <form action="">
+                    <label htmlFor="username">Username</label><br />
+                    <input type="text" name="username" id="username" placeholder="Username" /><br />
+                    <label htmlFor="password">Password</label><br />
+                    <input type="password" name="password" id="password" placeholder="Password" /><br />
+                    <button type="submit">Login</button>
+                    <p>
+                        Don't have an account? <Link to="/signup">SignUp</Link>
+                    </p>
+                </form>
+            </div>
         </>
     );
 }
+
 export default Login;
